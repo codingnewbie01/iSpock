497dc91c255e374103af6e022be55cb65766b03d
Arvind Verma <hey.arvind02@gmail.com>
2024-01-31 00:08:21 +0530

    Complete the 99.99% design

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("iSpock");
+
+ // Get all the navigation links
+
+ const links = document.querySelectorAll('#divLNav a');
+
+ // Add focus event listeners to each link
+
+ links.forEach(link => {
+     link.addEventListener('focus', () => {
+         link.classList.add('focused');
+         link.classList.remove('blurred');
+     });
+     
+     // Add blur event listeners to each link
+     link.addEventListener('blur', () => {
+         link.classList.remove('focused');
+         link.classList.add('blurred');
+     });
+ });
